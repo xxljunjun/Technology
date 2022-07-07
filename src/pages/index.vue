ee13bd66ee489250ff45f951d7786195bfbc852c
xxljunjun <793117268@qq.com>
2022-07-07 15:27:42 +0800

    项目初始化

---
+++ src/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+   1111111
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+    
+  },
+  data() {
+    return {
+      
+    }
+  },
+  mounted() {
+  
+  },
+  methods: {
+   
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.home{
+
+}
+</style>
