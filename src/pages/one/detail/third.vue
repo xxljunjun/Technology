afd78c509f1a40998bcc75be6c7c8d372a8f2e97
xxljunjun <793117268@qq.com>
2023-05-24 09:05:53 +0800

    图表

--- src/pages/one/detail/third.vue
+++ src/pages/one/detail/third.vue
@@ -1,19 +1,47 @@
 <template>
-  <div class="one">
-    333333
+  <div class="second">
+    <card title="盒子1">
+      <box1></box1>
+    </card>
+    <card title="盒子2">
+      <box1></box1>
+    </card>
+    <card title="盒子3">
+      <box1></box1>
+    </card>
+    <card title="盒子4">
+      <box1></box1>
+    </card>
+    <card title="盒子5">
+      <box1></box1>
+    </card>
+    <card title="盒子6">
+      <box1></box1>
+    </card>
   </div>
 </template>
 
 <script>
+import card from "@/components/card.vue"
+import box1 from "@/pages/one/detail/second/box_1.vue"
  export default { 
-    // beforeRouterEnter(to,from,next){
-    //   next({path:'/one/first'})
-    // }
+    components:{
+      card,
+      box1 
+
+    }
  }
 </script>
 
 <style lang="scss" scoped>
-.one{
-  
+.second{
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  padding: 50px 50px 0;
+  .card{
+    margin-bottom: 50px;
+  }
 }
 </style>
--- src/pages/three/index.vue
+++ src/pages/three/index.vue
@@ -1,16 +1,47 @@
 <template>
-
-  <div >
-    three
-
+  <div class="second">
+    <card title="盒子1">
+      <box1></box1>
+    </card>
+    <card title="盒子2">
+      <box1></box1>
+    </card>
+    <card title="盒子3">
+      <box1></box1>
+    </card>
+    <card title="盒子4">
+      <box1></box1>
+    </card>
+    <card title="盒子5">
+      <box1></box1>
+    </card>
+    <card title="盒子6">
+      <box1></box1>
+    </card>
   </div>
-
 </template>
 
 <script>
- export default { data() { return { } }, methods: { }, }
+import card from "@/components/card.vue"
+import box1 from "@/pages/one/detail/second/box_1.vue"
+ export default { 
+    components:{
+      card,
+      box1 
+
+    }
+ }
 </script>
 
 <style lang="scss" scoped>
-
+.second{
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  padding: 50px 50px 0;
+  .card{
+    margin-bottom: 50px;
+  }
+}
 </style>
